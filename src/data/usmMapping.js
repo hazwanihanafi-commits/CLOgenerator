178fdc2737158124e1db6362409f3d26d7f9d7c7
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-11-13 20:24:27 +0800

    Update usmMapping.js

--- src/data/usmMapping.js
+++ src/data/usmMapping.js
@@ -5,3 +5,17 @@ export const IEG = {
   IEG4: "Communication",
   IEG5: "Leadership, Teamwork & Lifelong Learning"
 };
+export const IEGtoPEO = {
+  IEG1: ["PEO1"],
+  IEG2: ["PEO2"],
+  IEG3: ["PEO3"],
+  IEG4: ["PEO1", "PEO4"],
+  IEG5: ["PEO3", "PEO4"]
+};
+export const PEOtoPLO = {
+  PEO1: ["PLO1", "PLO2", "PLO3", "PLO5"],
+  PEO2: ["PLO4", "PLO10"],
+  PEO3: ["PLO3", "PLO8", "PLO11"],
+  PEO4: ["PLO5", "PLO6", "PLO9", "PLO11"]
+};
+
