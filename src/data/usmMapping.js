292f26d1ffeef03e432442b29ffa352ab140dd36
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-11-13 20:19:24 +0800

    Create usmMapping.js

---
+++ src/data/usmMapping.js
@@ -0,0 +1,7 @@
+export const IEG = {
+  IEG1: "Knowledge & Professional Skills",
+  IEG2: "Values, Ethics & Humanity",
+  IEG3: "Socio-Entrepreneurship & Sustainability",
+  IEG4: "Communication",
+  IEG5: "Leadership, Teamwork & Lifelong Learning"
+};
